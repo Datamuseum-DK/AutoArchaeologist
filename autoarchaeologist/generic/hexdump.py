04781a3a6857bb0ee88fcd9375c3fd9263581198
Poul-Henning Kamp <phk@FreeBSD.org>
2020-11-14 22:00:27 +0000

    Optimize hexdump()

--- autoarchaeologist/generic/hexdump.py
+++ autoarchaeologist/generic/hexdump.py
@@ -4,28 +4,29 @@
 
 def hexdump(this, html=True, width=16, prefix=""):
     ''' Hexdump an artifact '''
-    total = len(this)
-    for i in range(0, total, width):
-        txt = prefix + "%04x " % i
-        for j in range(min(width, total - i)):
-            txt += " %02x" % this[i + j]
-        if j < (width - 1):
-            txt += "   " * ((width - 1) - j)
-        txt += "  |"
-        for j in range(min(width, total - i)):
-            k = this[i + j]
-            if html and k == 0x3c:
-                txt += "&lt;"
-            elif html and k == 0x26:
-                txt += "&amp;"
-            elif 32 <= k <= 126:
-                txt += "%c" % k
-            else:
-                txt += " "
-        if j < (width - 1):
-            txt += " " * ((width - 1) - j)
-        txt += "|"
-        yield txt
+    txt1 = ""
+    txt2 = ""
+    for n, x in enumerate(this):
+        i = n % width
+        if not i:
+            txt1 = prefix + "%04x " % n
+            txt2 = "  ┆"
+        txt1 += " %02x" % x
+        if html and x == 0x3c:
+            txt2 += "&lt;"
+        elif html and x == 0x26:
+            txt2 += "&amp;"
+        elif 32 <= x <= 126:
+            txt2 += "%c" % x
+        else:
+            txt2 += " "
+        if i == width - 1:
+            yield txt1 + txt2 + "┆"
+    i = len(this) % width
+    if i:
+        txt1 += "   " * (width - i)
+        txt2 += " " * (width - i)
+        yield txt1 + txt2 + "┆"
 
 def hexdump_to_file(this, fo, *args, **kwargs):
     ''' Hexdump to a file '''
