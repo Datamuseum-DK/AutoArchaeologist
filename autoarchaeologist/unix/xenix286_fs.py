2cd7451b03a7c044413ac74127a951155d93f6db
Poul-Henning Kamp <phk@FreeBSD.org>
2020-11-26 22:22:47 +0000

    Xenix 286 hackery

---
+++ autoarchaeologist/unix/xenix286_fs.py
@@ -0,0 +1,178 @@
+'''
+   Xenix 286 UFS fileystem with cylinder groups
+   --------------------------------------------
+'''
+
+import struct
+
+import autoarchaeologist.unix.unix_fs as ufs
+
+class SysVInode(ufs.Inode):
+    ''' System V inode '''
+
+    def __init__(self, ufs, **kwargs):
+        super().__init__(ufs, **kwargs)
+        self.fix_di_addr()
+
+    def fix_di_addr(self):
+        ''' convert .di_addr[] to .di_db[] and di.ib[] '''
+        raw = self.di_addr
+        tmp = []
+        if self.ufs.ENDIAN == "<":
+            for i in range(0, len(raw) - 3, 3):
+                tmp.append((raw[i + 2] << 16) | (raw[i + 1] << 8) | raw[i + 0])
+        else:
+            for i in range(0, len(raw) - 3, 3):
+                tmp.append((raw[i + 0] << 16) | (raw[i + 1] << 8) | raw[i + 2])
+
+        self.di_db = tmp[:-3]
+        self.di_ib = tmp[-3:]
+
+class Xenix_FileSystem(ufs.UnixFileSystem):
+    ''' System V Filesystem '''
+
+    FS_TYPE = "SysV Filesystem"
+    NICFREE = 50
+    NICINOD = 100
+    CHARSET = "ISO-8859-1"
+    INODE_SIZE = 0x40
+
+    BMAPSIZE = 994
+    MAXCGS = 80
+
+    CYLGRP_LAYOUT = (
+        ("cg_doffset", "1L"),
+        ("cg_ioffset", "1L"),
+        ("cg_dblocks", "1H"),
+        ("cg_ifirst", "1H"),
+        ("cg_number", "1B"),
+        ("cg_currextent", "1B"),
+        ("cg_lowat", "1H"),
+        ("cg_hiwat", "1H"),
+        ("cg_erotor", "1H"),
+        ("cg_ilock", "1B"),
+        ("cg_reserved", "9B"),
+        ("cg_bits", "%dB" % BMAPSIZE),
+    )
+
+    FILSYS_LAYOUT = (
+        ("fs_fname", "6B",),		# file system name
+        ("fs_fpack", "6B",),		# pack name
+        ("fs_fsize", "1L",),		# number of data blocks in fs
+        ("fs_cgblocks", "1H",),		# number of blocks per cg
+        ("fs_maxblock", "1L",),		# max disk block in fs
+        ("fs_cginodes", "1H",),		# number of inodes per cg
+        ("fs_maxino", "1H",),		# max inumber in fs
+        ("fs_time", "1L",),		# time last modified
+        ("fs_fmod", "1B",),		# modified flag
+        ("fs_ronly", "1B",),		# read-only fs
+        ("fs_clean", "1B",),		# fs was cleanly unmounted
+        ("fs_type", "1B",),		# fs type and version
+        ("fs_fnewcg", "1H",),		# contains FNEWCG
+        ("fs_snewcg", "1H",),		# contains SNEWCG
+        ("fs_ffree", "1L",),		# number of free data blocks in fs
+        ("fs_ifree", "1H",),		# number of free inodes in fs
+        ("fs_dirs", "1H",),		# number of directories in fs
+        ("fs_extentsize", "1B",),	# native extent size
+        ("fs_cgnum", "1B",),		# number of cg's in fs
+        ("fs_cgrotor", "1B",),		# next cg to be searched
+        ("fs_reserved", "15B",),	# reserved.
+    )
+
+    CGINFO_LAYOUT = (
+        ("fs_cgincore", "1H",),		# points to buf structure in core
+        ("fs_cgblk", "1L",),		# disk address of cg header
+        ("fs_cgffree", "1H",),		# number of free data blocks in cg
+        ("fs_cgifree", "1H",),		# number of free inodes in cg
+        ("fs_cgdirs", "1H",),		# number of directories in cg
+    )
+
+    INODE_LAYOUT = (
+        ("di_mode", "1H"),
+        ("di_nlink", "1H"),
+        ("di_uid", "1H"),
+        ("di_gid", "1H"),
+        ("di_size", "1L"),
+        ("di_addr", "40B"),
+        ("di_atime", "1L"),
+        ("di_mtime", "1L"),
+        ("di_ctime", "1L"),
+    )
+
+    MAGIC = {
+        bytes.fromhex("fd187e20"): ">",
+        bytes.fromhex("207e18fd"): "<",
+    }
+
+    def get_superblock(self):
+        ''' Read the superblock '''
+        if not self.this.has_type("XENIX Partition"):
+            return
+
+        self.SECTOR_SIZE = 1024
+        self.ENDIAN = "<"
+
+        sblock = ufs.Struct(
+            name="filsys",
+            **self.read_struct(
+                self.FILSYS_LAYOUT,
+                0,
+            )
+        )
+        if max(sblock.fs_fname):
+            return
+        if max(sblock.fs_fpack):
+            return
+        if max(sblock.fs_reserved):
+            return
+        if not sblock.fs_maxino:
+            return
+
+        sblock.cylinders = []
+        off = sblock._size
+        for i in range(0, self.MAXCGS):
+            cginfo = ufs.Struct(
+                name="cginfo",
+                **self.read_struct(
+                    self.CGINFO_LAYOUT,
+                    off,
+                )
+            )
+            if not cginfo.fs_cgblk:
+                break
+            cylgrp = ufs.Struct(
+                name="cylgrp",
+                offset=cginfo.fs_cgblk*self.SECTOR_SIZE,
+                **self.read_struct(
+                    self.CYLGRP_LAYOUT,
+                    (cginfo.fs_cgblk-1) * self.SECTOR_SIZE,
+                )
+            )
+            sblock.cylinders.append(cylgrp)
+            off += cginfo._size
+        
+        sblock.fs_nindir = self.SECTOR_SIZE // 4
+        sblock.fs_imax = sblock.fs_maxino - 1
+        self.sblock = sblock
+
+    def get_inode(self, inum):
+        ''' Return an Inode '''
+        cgn = inum // self.sblock.fs_cginodes
+        irem = inum % self.sblock.fs_cginodes
+        inoa = self.sblock.cylinders[cgn].offset
+        inoa += (irem - 1) * self.INODE_SIZE
+        if inoa > len(self.this) - 64:
+            return None
+        rv = SysVInode(
+            self,
+            name="sysv",
+            _offset=inoa,
+            di_inum=inum,
+            **self.read_struct(self.INODE_LAYOUT, inoa)
+        )
+        return rv
+
+    def get_block(self, blockno):
+        ''' Return a block '''
+        offset = (blockno-1) * self.SECTOR_SIZE
+        return self.this[offset:offset + self.SECTOR_SIZE]
---
+++ autoarchaeologist/unix/xenix286_partition.py
@@ -0,0 +1,57 @@
+'''
+   Xenix-286 partition
+   --------------------
+
+   There is no partition table, it's compiled into the kernel.
+
+   Heuristically hunt for filesystems.
+'''
+
+import struct
+
+ROOT_DIR_SIG = bytes().fromhex('''
+	02 00 2e 00 00 00 00 00  00 00 00 00 00 00 00 00
+''')
+
+assert len(ROOT_DIR_SIG) == 16
+
+class RC39_Partition():
+    ''' The sizes are hardcoded in the device driver source code '''
+
+    def __init__(self, this):
+        if this.top not in this.parents:
+            return
+
+        rootdirs = []
+        for i in range(0, len(this), 0x400):
+            if this[i:i+16].tobytes() == ROOT_DIR_SIG:
+                rootdirs.append(i)
+
+        lo = 0
+        parts = []
+        for i in rootdirs:
+            print(this, "Rootdir 0x%x" % i)
+            for j in range(i, lo, -0x400):
+                if this[j+1] not in (0x80, 0x81):
+                    continue
+                k = j - 0x800
+                if max(this[k:k+12]):
+                    continue
+                if not max(this[k + 12:k+16]):
+                    continue
+                print("   Could be 0x%x" % k)
+                print(this[k:k+64].hex())
+                lo = k + 0x800
+                parts.append(k)
+                break
+
+        print(this, parts)
+
+        if parts:
+            parts.append(len(this))
+            for i in range(0, len(parts)-1):
+                start=parts[i]
+                stop=parts[i+1]
+                print(this, "%x" % start, "%x" % stop)
+                y = this.create(start=start, stop=stop)
+                y.add_type("XENIX Partition")
