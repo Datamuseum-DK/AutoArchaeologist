2d6520a2640648fbf9c2375070ecf1b3efab9b9e
Poul-Henning Kamp <phk@FreeBSD.org>
2020-12-09 10:22:39 +0000

    Add a module to conveniently hexdump R1000 microcode

---
+++ autoarchaeologist/rational/r1k_ucode.py
@@ -0,0 +1,26 @@
+
+import autoarchaeologist.generic.hexdump as hexdump
+
+
+class R1K_Ucode_File():
+
+    def __init__(self, this):
+        good = False
+        for i in this.iter_types():
+            if i[-6:] == "_UCODE":
+                good = True
+        if not good:
+            return
+        this.add_type("UCODE")
+        this.add_interpretation(self, self.render_hexdump)
+
+
+    def render_hexdump(self, fo, this):
+        fo.write("<H4>HexDump</H4>\n")
+        fo.write("<pre>\n")
+        hexdump.hexdump_to_file(
+            this.tobytes(),
+            fo,
+            width=32
+        )
+        fo.write("</pre>\n")
