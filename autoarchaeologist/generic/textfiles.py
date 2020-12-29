a39b2f8da1266ca234d748989ecbd62f2fa82843
Poul-Henning Kamp <phk@FreeBSD.org>
2020-12-29 21:37:05 +0000

    Remove annoying debug print

--- autoarchaeologist/generic/textfiles.py
+++ autoarchaeologist/generic/textfiles.py
@@ -7,7 +7,6 @@ PATTERNS = {
 class TextFiles():
 
     def __init__(self, this):
-        print("?TF", this, this.has_note("ASCII"), this[:2].tobytes() == b'%!')
         if not this.has_note("ASCII"):
             return
         for k, v in PATTERNS.items():
