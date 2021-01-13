9297df38179e9342b380d6f8cdf2163286b2d81b
Poul-Henning Kamp <phk@FreeBSD.org>
2021-01-13 20:51:59 +0000

    Dont use tempfile, it is a memory pig.

--- autoarchaeologist/rational/r1k_assy.py
+++ autoarchaeologist/rational/r1k_assy.py
@@ -7,9 +7,9 @@
    (https://github.com/bsdphk/PyReveng3)
 '''
 
+import os
 import sys
 import subprocess
-import tempfile
 import html
 
 import autoarchaeologist
@@ -31,12 +31,10 @@ class R1K_Assy_File():
         if this[12] or this[13]:
             this.add_note("ELAB_segment_table")
 
-        tf1 = tempfile.NamedTemporaryFile()
-        this.writetofile(tf1)
-        tf1.flush()
+        tf1 = this.filename_for(suf=".bin.tmp")
+        this.writetofile(open(tf1.filename, "wb"))
 
-        self.tf2 = tempfile.NamedTemporaryFile(dir=this.top.html_dir)
-        # print("NAME", tf1.name, self.tf2.name)
+        self.tf2 = this.filename_for(suf=".dis.tmp")
         sys.stdout.flush()
         sys.stderr.flush()
         try:
@@ -44,21 +42,22 @@ class R1K_Assy_File():
                 [
                     "python3",
                     autoarchaeologist.PYREVENG3 + "/examples/R1000_400/example_ada.py",
-                    tf1.name,
-                    self.tf2.name,
+                    tf1.filename,
+                    self.tf2.filename,
                 ],
                 check = True,
             )
-        except subprocess.CalledProcessError as e:
-            print("Disassmbly failed", this, e)
+        except subprocess.CalledProcessError as err:
+            print("Disassmbly failed", this, err)
             return
         this.add_interpretation(self, self.render_disass)
+        os.remove(tf1.filename)
 
     def render_disass(self, fo, _this):
         ''' Emit PyReveng3's output '''
         fo.write("<H3>Disassembly</H3>\n")
         fo.write("<pre>\n")
-        for i in self.tf2:
-            j = i.decode("utf8")
-            fo.write(html.escape(j))
+        for i in open(self.tf2.filename):
+            fo.write(html.escape(i))
         fo.write("</pre>\n")
+        os.remove(self.tf2.filename)
