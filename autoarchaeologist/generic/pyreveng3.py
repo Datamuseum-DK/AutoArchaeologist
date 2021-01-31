681e9f1944889b8981c27ff2e4c93bab25c971be
Poul-Henning Kamp <phk@FreeBSD.org>
2021-01-31 09:18:37 +0000

    Define a canonical way of calling PyReveng3

---
+++ autoarchaeologist/generic/pyreveng3.py
@@ -0,0 +1,44 @@
+'''
+   Callout to PyReveng3 for disassembly
+   ------------------------------------
+'''
+
+import os
+import sys
+import subprocess
+
+import autoarchaeologist
+
+class PyReveng3():
+    ''' ... '''
+
+    def __init__(self, this, script):
+        if not autoarchaeologist.PYREVENG3:
+            return
+
+        tf1 = this.tmpfile_for()
+        this.writetofile(open(tf1.filename, "wb"))
+
+        tf2 = this.add_utf8_interpretation("Disassembly")
+        sys.stdout.flush()
+        sys.stderr.flush()
+
+        path = os.path.join(autoarchaeologist.PYREVENG3, script)
+
+        try:
+            subprocess.run(
+                [
+                    "python3",
+                    "-u",
+                    path,
+                    "-AutoArchaeologist",
+                    this.digest,
+                    tf1.filename,
+                    tf2.filename,
+                ],
+                check = True,
+            )
+        except subprocess.CalledProcessError as err:
+            print("Disassmbly failed", this)
+            print("    Script:", script)
+            print("    Error:", err)
