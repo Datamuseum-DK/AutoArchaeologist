87fcd93fa74c0aa78e8d745c9f2fd5c27b9d5481
Poul-Henning Kamp <phk@FreeBSD.org>
2021-03-03 20:39:11 +0000

    Use r1k_disass class.

--- autoarchaeologist/rational/r1k_experiment.py
+++ autoarchaeologist/rational/r1k_experiment.py
@@ -7,24 +7,12 @@
    (https://github.com/Datamuseum-DK/R1000.Disassembly)
 '''
 
-import os
-import sys
-import subprocess
-
-R1000DISASS = os.environ.get("AUTOARCHAEOLOGIST_R1000DISASS")
-if not R1000DISASS or not os.path.isdir(R1000DISASS):
-    R1000DISASS = str(os.environ.get("HOME")) + "/R1000.Disassembly/"
-if not R1000DISASS or not os.path.isdir(R1000DISASS):
-    R1000DISASS = str(os.environ.get("HOME")) + "/Proj/R1000.Disassembly/"
-if not R1000DISASS or not os.path.isdir(R1000DISASS):
-    R1000DISASS = None
+import autoarchaeologist.rational.r1k_disass as r1k_disass
 
 class R1kExperiment():
     ''' Diagnostic Experiments '''
 
     def __init__(self, this):
-        if R1000DISASS is None:
-            return
         dothis = False
         for t in (
            "M32",
@@ -41,29 +29,7 @@ class R1kExperiment():
         if not dothis:
             return
 
-        tf1 = this.tmpfile_for()
-        this.writetofile(open(tf1.filename, "wb"))
-
-        tf2 = this.add_utf8_interpretation("Disassembly")
-        sys.stdout.flush()
-        sys.stderr.flush()
-
-        path = os.path.join(R1000DISASS, "EXP", "disass_experiment.py")
-
-        try:
-            subprocess.run(
-                [
-                    "python3",
-                    "-u",
-                    path,
-                    "-AutoArchaeologist",
-                    this.digest[:16],
-                    tf1.filename,
-                    tf2.filename,
-                ],
-                check = True,
-            )
-        except subprocess.CalledProcessError as err:
-            print("Disassmbly failed", this)
-            print("    Path:", path)
-            print("    Error:", err)
+        r1k_disass.R1kDisass(
+            this,
+            "EXP/disass_experiment.py",
+        )
