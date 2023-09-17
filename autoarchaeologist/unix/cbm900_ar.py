988d3a4253fe97925ae9caf4f4bda31e76ae7d86
Poul-Henning Kamp <phk@FreeBSD.org>
2023-09-17 20:03:27 +0000

    Use octetview and namespace

--- autoarchaeologist/unix/cbm900_ar.py
+++ autoarchaeologist/unix/cbm900_ar.py
@@ -6,33 +6,79 @@ import struct
 
 import autoarchaeologist
 
-class Ar():
+from autoarchaeologist.generic import octetview as ov
+from autoarchaeologist.unix.unix_stat import UnixStat
+
+class ArHdr(ov.Struct):
+    ''' see ⟦309830465⟧ '''
+
+    def __init__(self, up, lo):
+        super().__init__(
+            up,
+            lo,
+            name_=ov.Text(14),
+            time_=ov.Le32,
+            gid_=ov.Le16,
+            uid_=ov.Le16,
+            mode_=ov.Le16,
+            size_=ov.Me32,
+        )
+        self.name.txt = self.name.txt.rstrip()
+
+class NameSpace(autoarchaeologist.NameSpace):
+    ''' ... '''
+
+    stat = UnixStat()
+
+    TABLE = (
+        ( "l", "mode"),
+        ( "r", "uid"),
+        ( "r", "gid"),
+        ( "r", "size"),
+        ( "r", "time"),
+        ( "l", "name"),
+        ( "l", "artifact"),
+    )
+
+    def ns_render(self):
+        arhdr = self.ns_priv
+        return [
+            self.stat.mode_bits(arhdr.mode.val),
+            arhdr.uid.val,
+            arhdr.gid.val,
+            arhdr.size.val,
+            self.stat.timestamp(arhdr.time.val),
+        ] + super().ns_render()
+
+class Ar(ov.OctetView):
 
     def __init__(self, this):
         if len(this) < 30:
             return
-        words = struct.unpack("<H", this[:2])
-        if words[0] != 0o177535:
+        super().__init__(this)
+        y = ov.Struct(
+            self,
+            0,
+            magic_=ov.Le16
+        )
+        if y.magic.val != 0o177535:
             return
-        self.this = this
+        y.insert()
         this.type = "Ar_file"
         this.add_note("Ar_file")
-        offset = 2
-        a = this.create(start=0,stop=2)
-        a.type = "AR magic"
+        offset = y.hi
+        self.namespace = NameSpace(
+            name='',
+            root=this,
+            separator="",
+        )
         while offset < len(this):
-            words = struct.unpack("<14sLHHHHH", this[offset:offset+28])
-            name = words[0].rstrip(b'\x00').decode("ASCII")
-            a = this.create(start=offset, stop=offset+28)
-            a.type = "AR header"
-            offset += 28
-            i = words[-2] << 16
-            i |= words[-1]
-            a = this.create(start=offset, stop=offset+i)
-            a.add_note("AR member")
-            try:
-                a.set_name(name)
-            except autoarchaeologist.DuplicateName:
-                a.add_note(name)
-            offset += i
-        this.add_interpretation(self, this.html_interpretation_children)
+            y = ArHdr(self, offset).insert()
+            offset = y.hi
+            if offset + y.size.val > len(this):
+                break
+            z = ov.This(self, lo=offset, width=y.size.val).insert()
+            offset = z.hi
+            NameSpace(y.name.txt, self.namespace, this=z.this, priv=y)
+        this.add_interpretation(self, self.namespace.ns_html_plain)
+        self.render()
