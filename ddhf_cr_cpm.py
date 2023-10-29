36011e88f9bb0dcbc6c7fb3a0f89ff9a375a13cf
Poul-Henning Kamp <phk@FreeBSD.org>
2023-10-29 15:18:05 +0000

    Collect all CR CP/M in one excavation

--- ddhf_cr_cpm.py
+++ ddhf_cr_cpm.py
@@ -1,5 +1,5 @@
 '''
-Christian Rovsing CR7 Artifacts from Datamuseum.dk's BitStore
+Christian Rovsing CR8 Artifacts from Datamuseum.dk's BitStore
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 '''
@@ -18,7 +18,7 @@ class TxtFile(textfiles.TextFile):
 
 cpm.cpm_filename_typecase.set_slug(0x5f, '_', '_')
 
-class CR7_Ds2089(type_case.DS2089):
+class CR8_Ds2089(type_case.DS2089):
     ''' ... '''
 
     def __init__(self):
@@ -34,21 +34,21 @@ class CR7_Ds2089(type_case.DS2089):
         #self.set_slug(0x19, ' ', '«eof»', self.EOF)
         #self.set_slug(0x7f, ' ', '')
 
-class CR7TypeCase(type_case.Ascii):
+class CR8TypeCase(type_case.Ascii):
 
     def __init__(self):
         super().__init__()
         self.set_slug(0x0d, ' ', '')
         self.set_slug(0x1a, ' ', '«eof»', self.EOF)
 
-class CR7(ddhf.DDHF_Excavation):
+class CR_CPM(ddhf.DDHF_Excavation):
 
-    ''' All CR7 artifacts '''
+    ''' All CR8 artifacts '''
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.type_case = CR7_Ds2089()
+        self.type_case = CR8_Ds2089()
 
         self.add_examiner(cpm.CpmFileSystem)
         self.add_examiner(TxtFile)
@@ -56,12 +56,14 @@ class CR7(ddhf.DDHF_Excavation):
 
         self.from_bitstore(
             "CR/CR7",
+            "CR/CR8",
+            "CR/CR16",
         )
 
 if __name__ == "__main__":
     ddhf.main(
-        CR7,
-        html_subdir="cr7",
-        ddhf_topic = 'Christian Rovsing CR7 "Diplomat"',
-        ddhf_topic_link = 'https://datamuseum.dk/wiki/CR7_Diplomat'
+        CR_CPM,
+        html_subdir="cr_cpm",
+        ddhf_topic = 'Christian Rovsing CR7, CR8 & CR16 CP/M',
+        ddhf_topic_link = 'https://datamuseum.dk/wiki/Christian_Rovsing_A/S'
     )
