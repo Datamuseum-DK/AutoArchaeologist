36d2b5c35af3598184624917b5c3a0cb52440cd4
Poul-Henning Kamp <phk@FreeBSD.org>
2024-04-01 17:54:00 +0000

    Refactor out a base-class for result HTML pages

---
+++ autoarchaeologist/base/result_page.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+'''
+    AutoArchaeologist Result Page
+    -----------------------------
+
+    `ResultPage` is a base-class for `Excavation` and `Artifact`
+    holding the common aspects related to HTML output generation
+'''
+
+from . import interpretation
+
+class ResultPage():
+    ''' ... '''
+
+    top = None
+
+    def __init__(self):
+        self.rp_interpretations = []
+        self.unique = 0
+        self.ns_roots = []
+
+    def get_unique(self):
+        ''' Return a unique (increasing) number '''
+        retval = self.unique
+        self.unique += 1
+        return retval
+
+    def add_interpretation(self, owner, func, more=False):
+        ''' Add an interpretation '''
+        self.rp_interpretations.append((owner, func, more))
+
+    def add_utf8_interpretation(self, title, **kwargs):
+        ''' Add early UTF-8 interpretation '''
+        return interpretation.Utf8Interpretation(self, title, **kwargs)
+
+    def add_html_interpretation(self, title, **kwargs):
+        ''' Add early HTML interpretation '''
+        return interpretation.HtmlInterpretation(self, title, **kwargs)
+
+    def tmpfile_for(self):
+        ''' create a temporary file '''
+        return self.top.filename_for(
+            self,
+            suf=".tmp.%d" % self.get_unique(),
+            temp=True
+        )
+
+    def emit_interpretations(self, fo, domore=False):
+        ''' emit registered interpretations '''
+        retval = False
+        for _owner, func, more in self.rp_interpretations:
+            if domore or not more:
+                fo.write('<div>\n')
+                func(fo, self)
+                fo.write('</div>\n')
+            if more:
+                retval = True
+        return retval
