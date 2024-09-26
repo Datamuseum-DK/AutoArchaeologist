1ddd8e6d4d01279421066bdfdd5e19ed60a795ea
Poul-Henning Kamp <phk@FreeBSD.org>
2024-09-26 14:36:07 +0000

    Modernize

--- autoarchaeologist/generic/samesame.py
+++ autoarchaeologist/generic/samesame.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# See LICENSE file for full text of license text
+
 '''
-   Artifacts with only one byte value
+   Artifacts where all bytes are identical
 '''
 
 class SameSame():
-    ''' Recognize artifacts with only a single byte value '''
+    ''' Tag artifacts with only a single byte value '''
     def __init__(self, this):
         i = this[0]
         for j in this:
@@ -11,14 +17,4 @@ class SameSame():
                 return
 
         self.this = this
-        note = "Boring" if this[0] else "Blank"
-        if not this.has_note(note):
-            this.add_type(note)
-            this.add_interpretation(self, self.html_as_interpretation)
-
-    def html_as_interpretation(self, fo, _this):
-        ''' Pseudo assembler syntax '''
-        fo.write("<H3>SameSame</H3>\n")
-        fo.write("<pre>\n")
-        fo.write("0x%02x[0x%x]" % (self.this[0], len(self.this)))
-        fo.write("</pre>\n")
+        this.add_type("SameSame")
