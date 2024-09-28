b7f909e41f43fb06cbf8fdd1b2891f66dd488a8f
Poul-Henning Kamp <phk@FreeBSD.org>
2024-09-28 17:30:12 +0000

    Fix license header braino

--- ddhf/ddhf_cbm900.py
+++ ddhf/ddhf_cbm900.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
-# See LICENSE file for full text of license text
+# See LICENSE file for full text of license
 
 '''
 Commodore CBM-900 Artifacts from Datamuseum.dk's BitStore
