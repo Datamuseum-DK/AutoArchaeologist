5ea7e30f2e49a2d7fe9c5cff14d725acba31aca2
Poul-Henning Kamp <phk@FreeBSD.org>
2024-09-29 08:31:45 +0000

    Improve the bitsavers collection

--- autoarchaeologist/collection/bitsavers_org.py
+++ autoarchaeologist/collection/bitsavers_org.py
@@ -8,22 +8,24 @@
    Download artifacts from bitsavers.org
 '''
 
+import os
+
+import urllib.request
+
+from ..base import excavation, decorator
+from ..container import imd_file
 
 PROTOCOL = "https"
 URL = "bitsavers.org/"
 
 IGNORE = (
     ".jpg",
+    ".txt",
+    ".zip",
 )
 
-import os
-import mmap
-import urllib.request
-
-from ..base import excavation, decorator
-from ..container import imd_file
-
 class FromBitsavers():
+    ''' Pull in artifacts from bitsavers.org '''
 
     def __init__(self, top, *args, media_types=None, cache_dir=None):
         self.top = top
@@ -40,39 +42,56 @@ class FromBitsavers():
         for i in args:
             self.process_arg(i)
 
-    def process_arg(self, arg):
-        arg = arg.strip('/')
-        if arg[0] == '-':
-            self.minuslist.add(arg[1:])
-            return
-        if arg in self.minuslist:
-            return
-        # print("ARG", arg)
-        if arg[-4:].lower() in IGNORE:
-            return
-        body = self.cache_fetch(arg)
-        if body is None:
-            return
-        assert len(body) > 0
-        if body[:15] == b'<!DOCTYPE HTML ':
-            self.process_dir(arg, body)
-            return
-        if arg[-4:].lower() in (".imd",):
-            # print("IMD ARTIFACT", arg)
-            artifact = imd_file.ImdContainer(octets = body)
-            try:
-                this = self.top.add_top_artifact(
-                    artifact,
-                    description="Bitsavers:" + arg
-                )
-            except excavation.DuplicateArtifact as why:
-                this = why.that
-            except:
-                raise
+    def cache_filename(self, key):
+        ''' return the cache file name for a given key '''
+        cache_file = os.path.join(self.cache_dir, key)
+        return cache_file
 
+    def resolve(self, arg):
+        ''' Resolve an argument (recursively) '''
+
+        for i in IGNORE:
+            if arg[-len(i):].lower() == i:
+                return
+
+        if arg.rstrip('/') in self.minuslist:
             return
 
-    def process_dir(self, arg, body):
+        cf = self.cache_filename(arg)
+        if os.path.isfile(cf):
+            yield cf
+            return
+
+        dcf = self.cache_filename(os.path.join(arg, "_.dir"))
+        if os.path.isdir(cf) and os.path.isfile(dcf):
+            yield from self.parse_dir(arg, dcf)
+            return
+
+        assert os.path.isdir(cf) or not os.path.exists(cf)
+
+        tcf = self.cache_filename(arg + ".tmp")
+        os.makedirs(os.path.dirname(tcf), exist_ok=True)
+        url = PROTOCOL + "://" + URL + arg
+        print("fetching", url)
+        with urllib.request.urlopen(url) as file:
+            body = file.read()
+        print("fetched", arg, len(body))
+        with open(tcf, "wb") as file:
+            file.write(body)
+
+        if body[:9] != b'<!DOCTYPE':
+            os.makedirs(os.path.dirname(cf), exist_ok=True)
+            os.rename(tcf, cf)
+            yield cf
+            return
+        os.makedirs(os.path.dirname(dcf), exist_ok=True)
+        os.rename(tcf, dcf)
+        yield from self.parse_dir(arg, dcf)
+
+    def parse_dir(self, arg, dcf):
+        ''' Parse a directory's HTML (crudely) '''
+        with open(dcf, "rb") as file:
+            body = file.read()
         for ptr in range(len(body) - 16):
             if body[ptr:ptr+16] != b'\n<tr><td><a href':
                 continue
@@ -94,39 +113,38 @@ class FromBitsavers():
                     continue
 
                 url = flds[0].decode('utf-8')
-                what = flds[1].decode('utf-8')
-                self.process_arg(os.path.join(arg, url))
+                yield from self.resolve(os.path.join(arg, url))
                 break
 
-
-    def cache_fetch(self, url):
-        cache_file = os.path.join(self.cache_dir, url.replace('/', '_'))
-        # print("CF", cache_file)
-        for turnus in range(2):
-            try:
-                fi = open(cache_file, "rb")
-                return mmap.mmap(
-                    fi.fileno(),
-                    0,
-                    access=mmap.ACCESS_READ,
-                )
-            except FileNotFoundError:
-                pass
-            print("fetching " + url)
-            url = PROTOCOL + "://" + URL + url
-            # print("URL", url)
-            body = urllib.request.urlopen(url).read()
-            with open(cache_file, "wb") as file:
-                file.write(body)
-        return None
+    def process_arg(self, arg):
+        ''' Process an argument '''
+        if arg[0] == '-':
+            self.minuslist.add(arg[1:].rstrip('/'))
+            return
+        for fn in self.resolve(arg):
+            if fn[-4:].lower() in (".imd",):
+                # print("IMD ARTIFACT", arg)
+                artifact = imd_file.ImdContainer(filename = fn)
+                try:
+                    self.top.add_top_artifact(
+                        artifact,
+                        description="Bitsavers:" + fn[len(self.cache_dir):]
+                    )
+                except excavation.DuplicateArtifact:
+                    pass
 
 class Decorator(decorator.Decorator):
     ''' ... '''
 
     def html_prefix_banner(self, file, _this):
         ''' Emit banner for the excavation '''
-        file.write('<H1>AutoArchaelogist excavation of Bitsavers.org artifacts</H1>\n')
-        file.write('<p>Bitsavers.org artifacts from <a href="https://bitsavers.org/">Bitsaver.org</a></p>\n')
-        file.write('<p>AutoArchaeologist software from <a href="https://github.com/Datamuseum-DK/AutoArchaeologist">DataMuseum.dk\'s github repo</a></p>\n')
-        file.write('<p>Contact: Poul-Henning Kamp</p>\n')
-        file.write('<hr/>\n')
+        file.write('''
+<H1>
+<a href="https://github.com/Datamuseum-DK/AutoArchaeologist">AutoArchaeologist</a>
+AutoArchaelogist
+excavation of
+<a href="https://bitsavers.org/">Bitsaver.org</a>
+artifacts
+</H1>
+<hr/>
+''')
