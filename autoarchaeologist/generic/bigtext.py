ab67348543a1a5624056bf557411d941bb1365ea
Poul-Henning Kamp <phk@FreeBSD.org>
2024-10-27 10:21:12 +0000

    Improve merging logic. Add more glyphs

--- autoarchaeologist/generic/bigtext.py
+++ autoarchaeologist/generic/bigtext.py
@@ -29,12 +29,13 @@ class Tree():
         self.tbl = {}
         self.term = None
         self.level = 0
+        self.mirror = 0
 
     def __str__(self):
         return "«%d %s %s»" % (
             self.level,
             str([self.term]),
-            "-".join("%02x" % x for x in self.tbl)
+            "-".join("%02x" % x for x in sorted(self.tbl))
         )
 
     def __getitem__(self, key):
@@ -44,7 +45,7 @@ class Tree():
         ''' Goto next branch '''
         return self.tbl.get(key)
 
-    def glyph_pattern(self, octets, txt):
+    def glyph_pattern(self, octets, txt, mirror=-1):
         ''' Add sequence of octets as glyph '''
         tree = self
         for n, i in enumerate(octets):
@@ -53,10 +54,21 @@ class Tree():
                 tree.tbl[i].level = n
             tree = tree[i]
         tree.term = txt
+        tree.mirror += mirror
+
+    def match(self, octets, depth=0):
+        ''' Attempt to match one of the glyphs '''
+        if len(octets) == 0:
+            return(depth, None, None)
+        i = self.tbl.get(octets[0])
+        if i:
+            return i.match(octets[1:], depth+1)
+        return (depth, self.term, self.mirror)
 
     def add_drawn_glyph(self, txt, *args):
         ''' Add a glyph to the recognizer tree '''
         # see bottom of this file for example use
+
         octets = [0] * len(args[0])
         bit = 1
         for i in args:
@@ -64,20 +76,30 @@ class Tree():
                 if j != ' ':
                     octets[n] |= bit
             bit <<= 1
-        self.glyph_pattern(octets, txt)
+        self.glyph_pattern(octets, txt, mirror=-1)
+
+        octets = [0] * len(args[0])
+        bit = 1
+        for i in reversed(args):
+            for n, j in enumerate(i):
+                if j != ' ':
+                    octets[n] |= bit
+            bit <<= 1
+        self.glyph_pattern(octets, txt, mirror=1)
 
 class Hit():
     ''' A hit of one or more glyphs '''
 
-    def __init__(self, up, first, last, txt):
+    def __init__(self, up, first, last, txt, mirror):
         self.up = up
         self.first = first
         self.last = last
         self.txt = txt
+        self.mirror = mirror
         self.lead = up.lead(self.first)
         self.tail = up.tail(self.last)
 
-    def __str__(self):
+    def __repr__(self):
         return '…'.join((
             hex(self.first),
             hex(self.last),
@@ -86,14 +108,22 @@ class Hit():
             str(self.tail),
         ))
 
+    def __len__(self):
+        return len(self.txt)
+
+    def cadence(self):
+        ''' estimate the width of the glyphs '''
+        return (self.last - self.first) / len(self.txt)
+
     def merge(self, other):
         ''' merge two hits, possibly with unrecognized stuff between '''
         between = bytes(self.up.this[self.last+1:other.first])
         self.last = other.last
-        if max(between):
+        if between and max(between):
             self.txt += '?'
         self.txt += other.txt
         self.tail = other.tail
+        self.mirror += other.mirror
 
     def find_leader(self, howfar):
         ''' Try to find a proper leader to the left '''
@@ -115,32 +145,45 @@ class Hit():
             if self.up.tail(self.last + i):
                 self.last += i
                 self.tail = True
-                self.txt += '@'
                 return
 
     def render(self):
         ''' render as Unicode '''
         bit = 0x01
-        yield '─' * (self.last - self.first)
-        while bit < 0x100:
-            if bit == 0x08:
-                yield '◦' * (self.last - self.first)
+        first = self.first - 10
+        last = self.last + 10
+        yield '─' * (last - first)
+        if self.mirror > 0:
+            order = (0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01)
+            xport = 0x04
+        else:
+            order = (0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80)
+            xport = 0x08
+        for bit in order:
+            if bit == xport:
+                yield '◦' * (last - first)
             l = []
-            for i in self.up.this[self.first:self.last]:
+            for i in self.up.this[first:last]:
                 if i & bit:
                     l.append('●')
                 else:
                     l.append(' ')
             yield ''.join(l)
-            bit <<= 1
-        yield '─' * (self.last - self.first)
+        yield '─' * (last - first)
 
     def commit(self):
         ''' Commit this hit as a new artifact '''
         this = self.up.this
-        that = this.create(start = self.first, stop = self.last)
+        first = self.first
+        while first > 0 and not this[first - 1]:
+            first -= 1
+        last = self.last
+        while last < len(this) and not this[last]:
+            last += 1
+        that = this.create(start = first, stop = last)
         that.add_type('BigText')
         that.add_note(self.txt)
+        that.add_type('»' + self.txt + '«')
         with that.add_utf8_interpretation("BigText") as file:
             for i in self.render():
                 file.write(i + '\n')
@@ -153,8 +196,8 @@ class BigTextBase():
 
     MIN_LENGTH = 3		# Minimum length of text
     MAX_UNKNOWN_LEN = 20	# Maximum amount of unrecognized input data
-    MIN_LEADER = 16		# Minimum blank leader on either side
-    GIVE_UP_AFTER = 1024	# Bail if nothing found by this much input
+    MIN_LEADER = 8		# Minimum blank leader on either side
+    GIVE_UP_AFTER = 8192	# Bail if nothing found by this much input
     VERBOSE = 0
 
     TREE = Tree()
@@ -175,30 +218,34 @@ class BigTextBase():
                 print(self.this, self.__class__.__name__, "HIT", hit)
 
         self.filter_hits()
+
+        if self.VERBOSE > 1:
+            for hit in self.hits:
+                print(self.this, self.__class__.__name__, "FILTERED", hit)
+
         if not self.hits:
             return
 
         for hit in self.hits:
             if self.VERBOSE:
                 print(this, self.__class__.__name__, "COMMIT", hit)
+            # Gobble up blank tape before/after ?
             hit.commit()
+            if self.VERBOSE > 1:
+                for i in hit.render():
+                    print(i)
 
     def find_hits(self):
         ''' Find all glyphs '''
-        first = 0
-        climb = self.TREE
-        for pos, i in enumerate(self.this):
-            j = climb.get(i)
+        for pos in range(len(self.this)):
+            if pos > 0 and self.this[pos-1] != 0:
+                continue
+            i, j, mirror = self.TREE.match(self.this[pos:])
+            #print("T", pos, i, j, self.GIVE_UP_AFTER, self.hits)
             if j:
-                if first == 0:
-                    first = pos
-                climb = j
-            else:
-                if climb.term:
-                    self.hits.append(Hit(self, first, pos, climb.term))
-                first = 0
-                climb = self.TREE
-            if pos > self.GIVE_UP_AFTER and not self.hits:
+                h = Hit(self, pos, pos + i, j, mirror)
+                self.hits.append(h)
+            elif pos > self.GIVE_UP_AFTER and len(self.hits) < self.MIN_LENGTH:
                 return
 
     def filter_hits(self):
@@ -216,19 +263,22 @@ class BigTextBase():
 
             # Try to merge with subsequent hits
             while n < len(self.hits) - 1:
-                if self.hits[n + 1].first - here.last > self.MAX_UNKNOWN_LEN:
+                between = self.this[here.last:self.hits[n + 1].first]
+                if between and max(between) > 0 and len(between) > self.MAX_UNKNOWN_LEN:
                     break
+                if between and max(between) == 0:
+                    here.txt += " " * int((len(between) / here.cadence()))
                 here.merge(self.hits.pop(n+1))
 
             # and a blank leader after
             here.find_trailer(self.MIN_LEADER)
-            if not here.tail:
+            if not here.tail and len(here) < self.MIN_LENGTH:
                 self.hits.pop(n)
                 continue
 
             # Must be this wide to ride
             if len(here.txt) < self.MIN_LENGTH:
-                self.hits.pop(0)
+                self.hits.pop(n)
                 continue
 
             n += 1
@@ -245,7 +295,28 @@ class BigTextBase():
 
 DEFAULT_GLYPH_TREE = Tree()
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '0',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('.',
+    '        ',
+    '        ',
+    '        ',
+    '        ',
+    '        ',
+    '        ',
+    '  xx    ',
+    '  xx    ',
+)
+DEFAULT_GLYPH_TREE.add_drawn_glyph('/',
+    '       x',
+    '      xx',
+    '     xx ',
+    '    xx  ',
+    '   xx   ',
+    ' xx     ',
+    'xx      ',
+    'x       ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('0',
     ' xx ',
     'x  x',
     'x  x',
@@ -256,7 +327,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '0',
     ' xx ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '1',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('0',
+    '  xx  ',
+    ' x  x ',
+    'xx  xx',
+    'xx  xx',
+    'xx  xx',
+    'xx  xx',
+    ' x  x ',
+    '  xx  ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('1',
     'xx ',
     ' x ',
     ' x ',
@@ -267,7 +349,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '1',
     'xxx',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '2',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('1',
+    '   x  ',
+    '  xx  ',
+    ' xxx  ',
+    'x xx  ',
+    '  xx  ',
+    '  xx  ',
+    '  xx  ',
+    'xxxxxx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('2',
     'xxx ',
     '   x',
     '   x',
@@ -278,7 +371,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '2',
     'xxxx',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '3',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('2',
+    '  xxxxx ',
+    ' xx   xx',
+    '      xx',
+    '      xx',
+    '     xx ',
+    ' x      ',
+    'xx      ',
+    'xxxxxxxx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('3',
     'xxxx',
     '  x ',
     ' x  ',
@@ -289,7 +393,29 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '3',
     'xxx ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '4',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('3',
+    'xxxx',
+    '  x ',
+    '    ',
+    'x x ',
+    ' x x',
+    '   x',
+    '   x',
+    'xxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('3',
+    ' xxxxx ',
+    'xx   xx',
+    '     xx',
+    '     xx',
+    '    xx ',
+    '     xx',
+    'xx   xx',
+    ' xxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('4',
     '   x',
     '  xx',
     ' x x',
@@ -300,7 +426,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '4',
     '   x',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '5',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('4',
+    'xx   xx ',
+    'xx   xx ',
+    'xx   xx ',
+    'xx   xx ',
+    'xxxxxxxx',
+    '     xx ',
+    '     xx ',
+    '     xx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('5',
     'xxxx',
     'x   ',
     'x   ',
@@ -311,7 +448,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '5',
     'xxxx',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '6',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('5',
+    'xxxxxxxx',
+    'xx      ',
+    'xx      ',
+    'xx xxx  ',
+    'xxx   x ',
+    '      xx',
+    '      xx',
+    ' xxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('6',
     ' xxx',
     'x   ',
     'x   ',
@@ -322,7 +470,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '6',
     ' xx ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '7',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('6',
+    '  xxxxxx',
+    ' xx     ',
+    'xx      ',
+    'xx xxx  ',
+    'xxx   x ',
+    'xx    xx',
+    'xx    xx',
+    ' xxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('7',
     'xxxx',
     '   x',
     '   x',
@@ -333,7 +492,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '7',
     'x   ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '8',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('7',
+    'xxxxxxxx',
+    'xx    xx',
+    '      xx',
+    '     xx ',
+    '     xx ',
+    '    xx  ',
+    '    xx  ',
+    '    xx  ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('8',
     ' xx ',
     'x  x',
     'x  x',
@@ -344,7 +514,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '8',
     ' xx ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '9',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('8',
+    ' xxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    ' xxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    ' xxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('9',
     ' xx ',
     'x  x',
     'x  x',
@@ -355,7 +536,18 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '9',
     'xx  ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '-',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('9',
+    ' xxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    ' xxxxxxx',
+    '      xx',
+    '      x ',
+    ' xxxxx  ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('-',
     '   ',
     '   ',
     '   ',
@@ -366,7 +558,7 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '-',
     '   ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '-',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('-',
     '    ',
     '    ',
     '    ',
@@ -377,7 +569,7 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '-',
     '    ',
 )
 
-DEFAULT_GLYPH_TREE.add_drawn_glyph( '*',
+DEFAULT_GLYPH_TREE.add_drawn_glyph('*',
     '    ',
     '    ',
     '    ',
@@ -386,6 +578,214 @@ DEFAULT_GLYPH_TREE.add_drawn_glyph( '*',
     'xxxx',
     ' xx ',
     'x  x',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('A',
+    ' xxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xxxxxxxx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('B',
+    'xxxxxxx ',
+    'xxxxxxxx',
+    'xx    xx',
+    'xx    xx',
+    'xxxxxxx ',
+    'xx    xx',
+    'xxxxxxxx',
+    'xxxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('C',
+    ' xxxxxx ',
+    'xx    xx',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xx    xx',
+    ' xxxxxx ',
+)
+DEFAULT_GLYPH_TREE.add_drawn_glyph('D',
+    'xxxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xxxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('E',
+    'xxxxxxxx',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xxxxxx  ',
+    'xx      ',
+    'xx      ',
+    'xxxxxxxx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('F',
+    'xxxxxxxx',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xxxxxx  ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('G',
+    ' xxxxxx ',
+    'xx    xx',
+    'xx      ',
+    'xx      ',
+    'xx  xxxx',
+    'xx    xx',
+    'xx    xx',
+    ' xxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('I',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('J',
+    '      xx',
+    '      xx',
+    '      xx',
+    '      xx',
+    '      xx',
+    '      xx',
+    'xx    xx',
+    ' xxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('L',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    'xxxxxxxx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('M',
+    'x      x',
+    'xx    xx',
+    'xxx  xxx',
+    'xxxxxxxx',
+    'xx xx xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('N',
+    'xx    xx',
+    'xxx   xx',
+    'xxxx  xx',
+    'xx xx xx',
+    'xx  xxxx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('O',
+    ' xxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    ' xxxxxx ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('P',
+    'xxxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xxxxxxx ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('S',
+    ' xxxxxx ',
+    'xx      ',
+    'xx      ',
+    'xx      ',
+    ' xxxxxx ',
+    '      xx',
+    '      xx',
+    ' xxxxxx ',
+)
+DEFAULT_GLYPH_TREE.add_drawn_glyph('R',
+    'xxxxxxx ',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xxxxxxx ',
+    'xx  xx  ',
+    'xx   xx ',
+    'xx    xx',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('T',
+    'xxxxxxxx',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+)
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('U',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    'xx    xx',
+    ' xxxxxx ',
+)
+
+
+DEFAULT_GLYPH_TREE.add_drawn_glyph('Y',
+    'x      x',
+    'xx    xx',
+    ' xx  xx ',
+    '  xxxx  ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
+    '   xx   ',
 )
 
 class BigText(BigTextBase):
