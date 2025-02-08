7621819055bbbcd68c6addea48c4da7e1444eceb
Poul-Henning Kamp <phk@FreeBSD.org>
2025-02-08 10:30:01 +0000

    Recognize D64 containers

--- autoarchaeologist/container/argv.py
+++ autoarchaeologist/container/argv.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# See LICENSE file for full text of license
 
 ''' Handle command line argument files '''
 
@@ -6,7 +10,9 @@ import os
 
 from . import plain_file
 from . import imd_file
+from . import d64_file
 from . import simh_tap_file
+# from . import floppytools
 
 def argv_file(excavation, fn):
     ''' Process extra command line arguments '''
@@ -14,10 +20,14 @@ def argv_file(excavation, fn):
         return None
     print("Loading", fn)
     ext = os.path.splitext(fn)
-    if ext[1] in (".tap", ".TAP"):
+    if ext[1] in (".tap", ".TAP",):
         this = simh_tap_file.SimhTapContainer(filename = fn)
-    elif ext[1] in (".imd", ".IMD"):
+    elif ext[1] in (".imd", ".IMD",):
         this = imd_file.ImdContainer(filename = fn)
+    elif ext[1] in (".d64", ".D64",):
+        this = d64_file.D64Container(filename = fn)
+    #elif ext[1] in (".cache",):
+    #    this = floppytools.FloppyToolsContainer(filename = fn)
     else:
         this = plain_file.PlainFileArtifact(fn)
     return excavation.add_top_artifact(this, description=fn, dup_ok=True)
