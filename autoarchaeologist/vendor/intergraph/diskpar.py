089e2972f090001e8776d34c299861534d6539fa
Poul-Henning Kamp <phk@FreeBSD.org>
2025-02-28 09:38:44 +0000

    Ref page number in doc.

--- autoarchaeologist/vendor/intergraph/diskpar.py
+++ autoarchaeologist/vendor/intergraph/diskpar.py
@@ -8,7 +8,7 @@
    Intergraph disk partitioning
    ----------------------------
 
-   see:
+   See pdf page 960 in :
       http://bitsavers.org/pdf/intergraph/clix/DSYS18412_Edition1_CLIX_Programmers_and_Users_Reference_Manual_199001.pdf
 
    Note that the stated magic number is wrong, it spells "sane" in
