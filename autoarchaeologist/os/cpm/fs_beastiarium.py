e519f16a80bb3702cc77711bcda57dad1686b9bb
Poul-Henning Kamp <phk@FreeBSD.org>
2025-04-13 08:40:28 +0000

    Polish the CPM beastiarium

--- autoarchaeologist/os/cpm/fs_beastiarium.py
+++ autoarchaeologist/os/cpm/fs_beastiarium.py
@@ -29,7 +29,7 @@ class CpmFSButler1a(fs_abc.CpmFileSystem):
     BLOCK_NO_WIDTH = 16
 
     def __init__(self, this, *args, **kwargs):
-        if not this.has_rec((100, 0, 1)):
+        if this._key_max[0] < 100:
             raise fs_abc.Nonsense
         super().__init__(this, *args, **kwargs)
 
@@ -44,11 +44,28 @@ class CpmFSButler1b(fs_abc.CpmFileSystem):
     BLOCK_NO_WIDTH = 16
 
     def __init__(self, this, *args, **kwargs):
-        if not this.has_rec((100, 0, 1)):
+        if this._key_max[0] < 100:
             raise fs_abc.Nonsense
         super().__init__(this, *args, **kwargs)
+
+class CpmFSCr16(fs_abc.CpmFileSystem):
+    ''' CR 16 '''
+
+    SECTOR_SIZE = 512
+    SECTORS = [1, 2, 3, 4, 5, 6, 7, 8]
+    TRACKS = [(c, 0) for c in range(1,77)] + [(c, 1) for c in range(76, 0, -1)]
+    BLOCK_SIZE = 2048
+    N_DIRENT = 64
+    BLOCK_NO_WIDTH = 16
+
+    def __init__(self, this, *args, **kwargs):
+        if not 70 < this._key_max[0] < 77:
+            raise fs_abc.Nonsense
+        super().__init__(this, *args, **kwargs)
+
 
 GEOMETRY_BEASTIARIUM = [
     CpmFSButler1a,
     CpmFSButler1b,
+    CpmFSCr16,
 ]
