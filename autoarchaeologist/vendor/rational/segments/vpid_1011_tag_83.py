cdb3efc02406eeec15ff743c6e4490c016aabb02
Poul-Henning Kamp <phk@FreeBSD.org>
2025-05-17 09:12:49 +0000

    Code segments state

--- autoarchaeologist/vendor/rational/segments/vpid_1011_tag_83.py
+++ autoarchaeologist/vendor/rational/segments/vpid_1011_tag_83.py
@@ -14,134 +14,169 @@
 	CODE_SEGMENT	1011
 	[…]
 
+   Layout based descriptions in pure segment ⟦ebef7d413⟧
 '''
 
 from ....base import bitview as bv
 from . import common as cm
 
-class CS54(bv.Struct):
-    def __init__(self, bvtree, lo):
-        super().__init__(
-            bvtree,
-            lo,
-            cs54_000_n_=-31,
-            cs54_001_n_=bv.Pointer.to(CS54),
-        )
-
-class CS55(bv.Struct):
-    def __init__(self, bvtree, lo):
-        super().__init__(
-            bvtree,
-            lo,
-            cs55_bitmap_n__=-16384,
-            cs55_040_n_=-20,
-            cs55_045_n_=-8,
-            cs55_049_n_=-31,
-        )
-
 class CodeSegHead(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10ac84c '''
+
     def __init__(self, bvtree, lo):
         super().__init__(
             bvtree,
             lo,
             vertical=True,
-            mgr_=cm.MgrHead,
-            hd_sh_=bv.Pointer.to(CodeSegSubHead),
-            hd_001_n_=-32,
-            hd_002_n_=bv.Pointer.to(CS62),
-            hd_003_n_=-32,
-            hd_004_n_=bv.Pointer.to(cm.BTree),
+            m000_n_=-31,
+            m001_n_=bv.Array(2, -32),
+            m002_p_=bv.Array(2, bv.Pointer),			#CS05
+            m003_p_=bv.Array(2, bv.Pointer.to(CS04)),		#CS04
+            m004_p_=bv.Array(2, bv.Pointer.to(CS03)),		#CS03
         )
 
-class CodeSegSubHead(bv.Struct):
+class CS03A(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10afe14 '''
+
+    def __init__(self, bvtree, lo):
+        super().__init__(
+            bvtree,
+            lo,
+            m000_b__=-0x4000,
+            m001_n_=-14,
+            m002_n_=-14,
+            m003_n_=-31,
+        )
+
+class CS03(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10af96c '''
+
     def __init__(self, bvtree, lo):
         super().__init__(
             bvtree,
             lo,
             vertical=True,
-            sh_001_n_=-32,
-            sh_002_n_=bv.Pointer.to(CS58),
-            sh_003_n_=-1,
-            sh_004_n_=-32,
-            sh_005_n_=bv.Pointer.to(CS59),
+            m000_a_=bv.Array(0x2ff, CS03A, vertical=True),
+            m001_p_=bv.Pointer.to(CS06),
         )
 
+class CS04A(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10b14c4 '''
 
-class CS58(bv.Struct):
     def __init__(self, bvtree, lo):
         super().__init__(
             bvtree,
             lo,
             vertical=True,
-            cs58_000_n_=bv.Array(1009, bv.Pointer.to(CS59), vertical=True),
+            m000_n_=-2,
+            m001_p_=bv.Pointer.to(CS07), 			#CS07
         )
 
-class CS59(bv.Struct):
-    def __init__(self, bvtree, lo):
-        super().__init__(
-            bvtree,
-            lo,
-            cs59_000_n_=-32,
-            cs59_001_n_=-32,
-            cs59_002_n_=bv.Pointer.to(CS60),
-            cs59_003_n_=bv.Pointer.to(CS59),
-            cs59_004_n_=bv.Pointer.to(CS59),
-            cs59_005_n_=-1,
-        )
+class CS04(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10b0ea4 '''
 
-class CS60(bv.Struct):
     def __init__(self, bvtree, lo):
         super().__init__(
             bvtree,
             lo,
             vertical=True,
-            cs60_000_n_=-24,
-            cs60_010_n_=-24,
-            cs60_020_n_=cm.SegId,
-            cs60_021_n_=-32,
-            cs60_060_n_=-3,
-
-            cs60_066_n_=cm.ObjRef,
-            cs60_067_n_=cm.ObjRef,
-            cs60_078_n_=-32,
-            cs60_079_n_=-32,
-            cs60_080_n_=cm.TimedProperty,
-            cs60_081_n_=cm.TimedProperty,
-            cs60_082_n_=cm.TimedProperty,
-            cs60_083_n_=cm.TimedProperty,
-            #cs60_090_n_=-73,
-            cs60_097_n_=-16,
-            cs60_098_n_=-19,
-            cs60_099_n_=-32,
+            m000_p_=bv.Pointer.to(CS07),			#CS07
+            m001_p_=bv.Pointer.to(CS08), 			#CS08
+            m002_n_=-31,
+            m003_n_=CS04A,
         )
 
-class CS62(bv.Struct):
+class CS06(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10b1bbc '''
+
+    def __init__(self, bvtree, lo):
+        super().__init__(
+            bvtree,
+            lo,
+            m000_n_=-31,
+            m001_p_=bv.Pointer.to(CS06),
+        )
+
+class CS07(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10b2174 '''
+
+    def __init__(self, bvtree, lo):
+        super().__init__(
+            bvtree,
+            lo,
+            m000_n_=bv.Array(2, -32),
+            m001_p_=bv.Pointer.to(CS09),
+            m002_p_=bv.Pointer.to(CS07),
+            m003_p_=bv.Pointer.to(CS07),
+            m004_p_=-1,
+        )
+
+class CS08(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10b2ccc '''
+
     def __init__(self, bvtree, lo):
         super().__init__(
             bvtree,
             lo,
             vertical=True,
-            cs62_000_n_=bv.Array(0x2ff, CS55, vertical=True),
-            cs62_001_n_=bv.Pointer.to(CS54),
+            m000_a_=bv.Array(0x3f1, bv.Pointer.to(CS07), vertical=True, elide=(0,)),
+        )
+
+class CS09A(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10b345c '''
+
+    def __init__(self, bvtree, lo):
+        super().__init__(
+            bvtree,
+            lo,
+            vertical=True,
+            m000_n_=-24,
+            m001_n_=-24,
+            m002_p_=bv.Array(2, -32),
+            m003_n_=-1,
+            m004_n_=-1,
+            m005_n_=-1,
+            m006_n_=cm.ObjRef,
+            m007_n_=cm.ObjRef,
+            m008_n_=-32,
+            m009_n_=-32,
+            m010_n_=cm.TimedProperty,
+            m011_n_=cm.TimedProperty,
+            m012_n_=cm.TimedProperty,
+            m013_n_=cm.TimedProperty,
+            m014_n_=-16,
+            m015_n_=-16,
+        )
+
+class CS09(bv.Struct):
+    ''' ⟦ebef7d413⟧ @ 0x10b331c '''
+
+    def __init__(self, bvtree, lo):
+        super().__init__(
+            bvtree,
+            lo,
+            vertical=True,
+            m000_s_=CS09A,
+            m001_n_=-3,
+            m002_n_=-32,
         )
 
 class V1011T83(cm.ManagerSegment):
+    ''' Code Segment Manager Segment - VPID 1011 - TAG 0x83 '''
 
     VPID = 1011
     TAG = 0x83
     TOPIC = "Code_Segment"
 
     def spelunk_manager(self):
-       '''...'''
+        head = CodeSegHead(self, self.seg_head.hi).insert()
+        bv.Pointer(self, head.hi, target=cm.BTree).insert()
 
-       CodeSegHead(self, 0xa1).insert()
-       
-       if False:
-           b = 0x0c07326
-           n = 0x2fd
-           t = b - n * 16443
-           t=0x1560d1d
-           for a in range(t, t + 1):
-           #for a in range(0x000429c, t):
-               for n in self.find_all(a):
-                   print("NN", hex(a), hex(n))
+        for t in (
+            0x11e1e7e,
+            0x11e482a,
+            0x14a6c30,
+            0x14b49db,
+        ):
+            for i in self.find_all(t):
+                print("FF", hex(i), "->", hex(t))
