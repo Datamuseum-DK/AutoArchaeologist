7bf2cc3eae0f6fb0526b3666c146cd252b0dfdfc
Poul-Henning Kamp <phk@FreeBSD.org>
2025-11-28 22:42:05 +0000

    R1K: Call the UCODE disassembler

--- autoarchaeologist/vendor/rational/r1k_ucode.py
+++ autoarchaeologist/vendor/rational/r1k_ucode.py
@@ -6,25 +6,23 @@
 
 from ...generic import hexdump
 
+from . import r1k_disass
+
 class R1K_Ucode_File():
 
     def __init__(self, this):
+
         good = False
         for i in this.iter_types():
             if i[-6:] == "_UCODE":
                 good = True
         if not good:
             return
+
+        print(this, "UCODE")
         this.add_type("UCODE")
-        this.add_interpretation(self, self.render_hexdump)
 
-
-    def render_hexdump(self, fo, this):
-        fo.write("<H4>HexDump</H4>\n")
-        fo.write("<pre>\n")
-        hexdump.hexdump_to_file(
-            this.tobytes(),
-            fo,
-            width=32
+        r1k_disass.R1kDisass(
+            this,
+            "UCODE/disass_ucode.py",
         )
-        fo.write("</pre>\n")
