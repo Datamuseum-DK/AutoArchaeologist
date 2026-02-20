1ac2eeec99e5a309ae7509f78105137733aa8816
Poul-Henning Kamp <phk@FreeBSD.org>
2026-02-20 16:43:40 +0000

    Attempt to figure out block_size dynamically

--- autoarchaeologist/vendor/apollo/apollo_domain_fs.py
+++ autoarchaeologist/vendor/apollo/apollo_domain_fs.py
@@ -10,10 +10,38 @@
 
 ''' ... '''
 
+import math
+
 from ...base import octetview as ov
 from ...base import namespace as ns
 
 UNREAD = b'_UNREAD_' * (4096//8)
+
+def ranges(numbers):
+    ''' Reduce list of numbers to ranges '''
+
+    diff = None
+    for i,j in enumerate(sorted(numbers)):
+        if i - j != diff:
+            if diff is not None:
+                yield first,last
+            first = j
+            diff = i -j
+        last = j
+    if diff is not None:
+        yield first,last
+
+def make_ranges(data):
+    ''' Eliminate small ranges '''
+    for low, high in ranges(data):
+        if low == high:
+            yield low, low
+        elif low + 1 == high:
+            yield low, low
+            yield high, high
+        else:
+            yield low, high
+
 
 class Uid(ov.Be64):
 
@@ -113,35 +141,6 @@ class Directory(ov.Struct):
 
         self.done()
 
-class PvLabelT(ov.Struct):
-    '''
-       ref: #1/p57
-       ref: #2/p54
-    '''
-    def __init__(self, tree, lo):
-        super().__init__(
-            tree,
-            lo,
-            version_=ov.Be16,
-            apollo_=ov.Text(6),
-            name_=ov.Text(32),
-            id_=Uid,
-            f30_=ov.Be16,
-            dtype_=ov.Be16,
-            blocks_per_pvol_=ov.Be32,
-            blocks_per_track_=ov.Be16,
-            blocks_per_cyl_=ov.Be16,
-            lv_list_=ov.Array(10, ov.Be32, vertical=True),
-            alt_lv_list_=ov.Array(10, ov.Be32, vertical=True),
-            phys_badspot_daddr_=ov.Be32,
-            phsy_diag_daddr_=ov.Be32,
-            phys_sector_start_=ov.Be16,
-            phys_sector_size_=ov.Be16,
-            pre_comp_=ov.Be16,
-
-            vertical=True,
-        )
-
 class VtocMapE(ov.Struct):
     '''
        ref: #2/p57
@@ -175,22 +174,29 @@ class VtocHdrT(ov.Struct):
             vertical=True,
         )
 
-class VtocCeHdrT(ov.Struct):
+
+class Indir(ov.Struct):
     '''
-       ref: #2/p56
     '''
     def __init__(self, tree, lo):
         super().__init__(
             tree,
             lo,
-            f000_=ov.Be32,
-            f001_=ov.Be32,
-            f004_=ov.Array(8, VtocEntryHeader, vertical=True, elide=(0,)),
-            fe88__=368,
-            fff8_=ov.Be32,
-            blkno_=ov.Be32,
+            bnos_=ov.Array(4096//4, ov.Be32, vertical=True, elide=(0,)),
             vertical=True,
         )
+
+    def render(self):
+        l = list(x.val for x in self.bnos)
+        while l and l[-1] == 0:
+            l.pop(-1)
+        yield "Indir {"
+        for lo,hi in make_ranges(l):
+            if lo == hi:
+                yield "  0x%08x," % lo
+            else:
+                yield "  0x%08x…0x%08x" % (lo,hi)
+        yield "}"
 
 class Tail(ov.Opaque):
     ''' ... '''
@@ -199,6 +205,8 @@ class VtocEntryHeader(ov.Struct):
     '''
        Name adopted from ref: #3
     '''
+    SIZEOF = 464
+
     def __init__(self, tree, lo):
         super().__init__(
             tree,
@@ -246,10 +254,6 @@ class VtocEntryHeader(ov.Struct):
         except Exception as err:
             print(self.tree.this, "COMMIT FAILED", hex(self.lo), err)
 
-    def render(self):
-        yield "YYY 0x%08x " % self.lo + str(self.tree.vtocx.get(self.obj.val)) + " " + str(self.committed)
-        yield from super().render()
-
     def iter_frag(self):
         self.left = self.length.val
 
@@ -261,16 +265,16 @@ class VtocEntryHeader(ov.Struct):
                 if lba == 0:
                     print(self.tree.this, hex(self.lo), "ZERO_BNO", hex(self.left))
                 lba &= 0x7fffffff
-                if lba >= len(self.tree.this) >> 12:
+                if lba >= len(self.tree.this) // self.tree.block_size:
                     print(self.tree.this, hex(self.lo), "BOGO_BNO", hex(self.left), hex(lba))
                     return
                 if self.left == 0:
                     return
-                if self.left >= 1<<12:
-                    yield lba<<12, (lba+1) << 12
-                    self.left -= 1<<12
+                if self.left >= self.tree.block_size:
+                    yield lba * self.tree.block_size, (lba+1) * self.tree.block_size
+                    self.left -= self.tree.block_size
                 else:
-                    yield lba<<12, (lba<<12) + self.left
+                    yield lba * self.tree.block_size, lba * self.tree.block_size + self.left
                     self.left = 0
                     return
 
@@ -279,17 +283,17 @@ class VtocEntryHeader(ov.Struct):
             return
 
         def get_indir(bno):
-            lba = bno << 12
+            lba = bno * self.tree.block_size
             if lba == 0:
                 print(self.tree.this, hex(self.lo), "BOGO_INDIR", hex(self.left), self.indir_blk, bno)
                 return None
-            iblk = ov.Array((1<<12)//4, ov.Be32, elide=(0,), vertical=True)(self.tree, lba).insert()
+            iblk = Indir(self.tree, lba).insert()
             return iblk
 
         ib = get_indir(self.indir_blk[0].val)
         if not ib:
             return
-        yield from work_through(ib)
+        yield from work_through(ib.bnos)
         if self.left == 0:
             return
 
@@ -297,11 +301,11 @@ class VtocEntryHeader(ov.Struct):
         ib2 = get_indir(self.indir_blk[1].val)
         if not ib2:
             return
-        for bno in ib2:
+        for bno in ib2.bnos:
             ib = get_indir(bno.val)
             if not ib:
                 return
-            yield from work_through(ib)
+            yield from work_through(ib.bnos)
             if self.left == 0:
                 return
         print(self.tree.this, hex(self.lo), "Need Indir3", hex(self.left), str(self.indir_blk))
@@ -309,7 +313,7 @@ class VtocEntryHeader(ov.Struct):
     def commit(self):
         self.committed = True
         #print(self.tree.this, "C", hex(self.lo), self.obj, self.system_type.val)
-        if (self.length.val + 0xfff) >> 12 > self.n_blk.val:
+        if (self.length.val + 0xfff) // self.tree.block_size > self.n_blk.val:
             print(self.tree.this, hex(self.lo), "NOT ENOUGH BLOCKS?", hex(self.length.val), hex(self.n_blk.val), self.obj, self.obj_typ)
             return
         if self.system_type.val in (1, 2,):
@@ -327,8 +331,8 @@ class VtocEntryHeader(ov.Struct):
                 r.append(self.tree.this[lo:hi])
                 y = ov.Opaque(self.tree, lo=lo, hi=hi).insert()
                 y.rendered = "Data " + str(self.obj)
-                if hi < lo + (1<<12):
-                    Tail(self.tree, lo=hi, hi=lo + (1<<12)).insert()
+                if hi < lo + self.tree.block_size:
+                    Tail(self.tree, lo=hi, width=self.tree.block_size).insert()
             if r:
                 y = self.tree.this.create(records=r)
                 ns.NameSpace(name=str(self.obj), parent=self.tree.ns, this=y)
@@ -344,19 +348,44 @@ class VtocEntryHeader(ov.Struct):
         else:
             print(self.tree.this, hex(self.lo), "WHAT?", hex(self.system_type.val), self.obj, self.obj_typ)
 
-class xVtocEntry(ov.Struct):
+    def render(self):
+        if self.committed:
+            yield " ".join(
+                (
+                "YYY 0x%08x" % self.lo,
+                str(self.tree.vtocx.get(self.obj.val)),
+                str(self.committed),
+                "%d" % len(self),
+                )
+            )
+            yield from super().render()
+        elif not sum(self):
+            yield "VtocEntryHeader { Ø }"
+        else:
+            yield "VtocEntryHeader { " + str(self.obj) + " uncommitted }"
+
+class VtocCeHdrT(ov.Struct):
     '''
-       ref: #1/p67
+       ref: #2/p56
     '''
     def __init__(self, tree, lo):
+        n = (tree.block_size - 4 * 4) // VtocEntryHeader.SIZEOF
         super().__init__(
             tree,
             lo,
-            f00_=VtocCeHdrT,
-            file_map_=ov.Array(0x23, ov.Be32, vertical=False),
-            vertical=False,
+            f000_=ov.Be32,
+            f001_=ov.Be32,
+            f004_=ov.Array(n, VtocEntryHeader, vertical=True, elide=(0,)),
+            vertical=True,
+            more=True,
         )
-
+        if len(self) < tree.block_size - 8:
+            self.add_field("pad_", tree.block_size - (len(self) + 8))
+        self.add_field("magic", ov.Be32)
+        self.add_field("blkno", ov.Be32)
+        self.done()
+        if self.blkno.val * tree.block_size != self.lo:
+            print(tree.this, "BAD BLOCKNO in VtocCeHdrT", hex(self.lo), hex(self.blockno))
 
 class VtocBucketEntry(ov.Struct):
     '''
@@ -371,43 +400,97 @@ class VtocBucketEntry(ov.Struct):
         )
         self.val = sum(self)
 
-
 class VtocBucket(ov.Struct):
     '''
        Name adopted from ref: #3
+
+       Twenty is the optimal choice for 1K block_size, if we assume
+       an 8 byte overhead both here and in the ``VtocBucketBlock``
+       struct.
+
     '''
+    SIZEOF = 8 + 20 * 12
+
     def __init__(self, tree, lo):
         super().__init__(
             tree,
             lo,
-            f00_=ov.Be64,
-            buckets_=ov.Array(0x14, VtocBucketEntry, vertical=True, elide=(0,)),
+            vtb_f00_=ov.Be64,
+            buckets_=ov.Array(20, VtocBucketEntry, vertical=True, elide=(0,)),
             vertical=True,
         )
+        self.val = sum(self)
         for v in self.buckets:
             if v.uid.val:
                 self.tree.vtocx[v.uid.val] = v.vtocx.val
 
+    def render(self):
+        if sum(self):
+            yield from super().render()
+        else:
+            yield "VtocBucket {Ø}"
+
 class VtocBucketBlock(ov.Struct):
     '''
        Name adopted from ref: #3
+
+       The magic number (0xfedca984) and blockno at the tail is
+       probably there for recovery purposes.
     '''
+    def __init__(self, tree, lo):
+        n_buckets = min(16, (tree.block_size-8) // VtocBucket.SIZEOF)
+        super().__init__(
+            tree,
+            lo,
+            buckets_=ov.Array(n_buckets, VtocBucket, vertical=True, elide=(0,)),
+            vertical=True,
+            more=True,
+        )
+        if len(self) < tree.block_size - 8:
+            self.add_field("pad_", tree.block_size - (len(self) + 8))
+        self.add_field("magic", ov.Be32)
+        self.add_field("blkno", ov.Be32)
+        self.done()
+        if self.blkno.val * tree.block_size != self.lo:
+            print(tree.this, "BAD BLOCKNO in VtocBucketBlock", hex(self.lo), hex(self.blockno))
+
+class Bat(ov.Dump):
+    ''' ... '''
+
+class BatHdrT(ov.Struct):
+    '''
+       Block Availability Table Header
+       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       ref: #1/p60
+       ref: #2/p37
+
+    '''
+
     def __init__(self, tree, lo):
         super().__init__(
             tree,
             lo,
-            buckets_=ov.Array(16, VtocBucket, vertical=True),
-            ff80_=120,
-            fff8_=ov.Be32,
-            blockno_=ov.Be32,
+            n_blk_=ov.Be32,
+            n_free_=ov.Be32,
+            addr_=ov.Be32,
+            base_add_=ov.Be32,
+            vcd_=ov.Be16,
+            bat_step_=ov.Be32,
             vertical=True,
+            more=True,
         )
-        if self.blockno.val<<12 != self.lo:
-            print(tree.this, "BAD BLOCKNO in VtocBucketBlock", hex(self.lo))
+        self.done(-0x20)
 
 class LvLabelT(ov.Struct):
     '''
+       Logical Volume Label
+       ~~~~~~~~~~~~~~~~~~~~~
+       ref: #1/p58
        ref: #2/p52
+
+       Not obvious how this scales with larger blocks, maybe the
+       bad_spot_list absorbs the extra space?
+
     '''
     def __init__(self, tree, lo):
         super().__init__(
@@ -417,8 +500,7 @@ class LvLabelT(ov.Struct):
             f02_=ov.Be16,
             name_=ov.Text(32),
             id_=Uid,
-            bat_hdr_=0x20,
-            #vtoc_hdr_=0x64,
+            bat_hdr_=BatHdrT,
             vtoc_hdr_=VtocHdrT,
             label_write_time_=ov.Be32,
             last_mounted_node_=ov.Be32,
@@ -442,35 +524,56 @@ class LvLabelT(ov.Struct):
             vertical=True,
         )
 
+class FreeBlock(ov.Opaque):
+    ''' ... '''
+
 class ApolloDomainLogicalVolume(ov.OctetView):
 
+    '''
+       Apollo Domain Logical Volume
+       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    '''
+
     def __init__(self, this):
-        if not this.has_type("ApolloDomainLogicalVolume"):
+        n = this.has_type("ApolloDomainLogicalVolume")
+        if n is None:
             return
         super().__init__(this)
+        print(this, "ApolloDomainLogicalVolume", n)
+        self.block_size = max(x.get('block_size', 0) for x in n)
 
         self.ns = ns.NameSpace(name='', root=this, separator='')
         lvl1 = LvLabelT(self, 0x0).insert()
-        lvl2 = LvLabelT(self, len(this) - (1<<12)).insert()
+        lvl2 = LvLabelT(self, len(this) - self.block_size).insert()
+
+        self.bat_hdr = lvl1.bat_hdr
+
+        self.bat_add = self.bat_hdr.base_add.val
+        self.bat_base = self.bat_hdr.addr.val * self.block_size
+        i = self.bat_hdr.n_blk.val / (8 * self.block_size)
+        if i > int(i):
+            i += 1
+        i = int(i)
+
+        if False:
+            for o in range(0, i * self.block_size, 0x20):
+                Bat(self, lo=self.bat_base + o, width=0x20).insert()
+        else:
+            Bat(self, lo=self.bat_base, width=i * self.block_size).insert()
 
         self.vtocx = {}
 
-        if False:
-            for n in range(lvl1.vtoc_hdr.map[0].lt_blk.val):
-                p = (lvl1.vtoc_hdr.map[0].blk_add.val+n)<<12
-                y = VtocBucketBlock(self, p).insert()
-        else:
-            ov.Array(
-                lvl1.vtoc_hdr.map[0].lt_blk.val,
-                VtocBucketBlock,
-                vertical=True,
-            )(self, lvl1.vtoc_hdr.map[0].blk_add.val<<12).insert()
+        ov.Array(
+            lvl1.vtoc_hdr.map[0].lt_blk.val,
+            VtocBucketBlock,
+            vertical=True,
+        )(self, lvl1.vtoc_hdr.map[0].blk_add.val * self.block_size).insert()
 
         vtocce = {}
         for uid, vtocx in self.vtocx.items():
             if not vtocx:
                 continue
-            lba = (vtocx & ~0xf) << 8
+            lba = (vtocx >> 4) * self.block_size
             i = vtocce.get(lba)
             if i is None:
                 i = VtocCeHdrT(self, lba).insert()
@@ -478,38 +581,141 @@ class ApolloDomainLogicalVolume(ov.OctetView):
             j = i.f004[vtocx & 0xf]
             j.commit()
 
+        if True:
+            nfree = 0
+            nused = 0
+            nwrong = 0
+            for bno in range(self.bat_add, self.bat_add + self.bat_hdr.n_blk.val):
+                l = list(self.find(bno * self.block_size, (bno+1) * self.block_size))
+                f = self.bfree(bno)
+                if f:
+                    nfree += 1
+                else:
+                    nused += 1
+                if f and len(l) == 0:
+                    FreeBlock(self, lo=bno * self.block_size, width=self.block_size).insert()
+                elif not f and len(l) > 0:
+                    pass
+                else:
+                    print(self.this, "? 0x%08x" % (self.block_size * bno), f, l)
+                    nwrong += 1
+            print(self.this, "Blocks marked free:", nfree, "marked used:", nused, "wrong:", nwrong)
+
+        if True:
+            for lo, hi in self.gaps():
+                lo += self.block_size - 1
+                lo -= lo % self.block_size
+                for ptr in range(lo, hi, self.block_size):
+                    if self.bfree(ptr // self.block_size):
+                        FreeBlock(self, lo=ptr, width=self.block_size).insert()
+
         this.add_interpretation(self, self.ns.ns_html_plain)
-        #this.add_interpretation(self, this.html_interpretation_children)
         self.add_interpretation(title="ApolloDomainLogicalVolume", more=True)
 
+    def bfree(self, bno):
+        ''' Is block free ? '''
+
+        bno -= self.bat_add
+        if bno < 0:
+            return None
+
+        p = self.bat_base + 4 * (bno // 32)
+        w = ov.Be32(self, p).val
+        m = 0x1 << (bno & 0x1f)
+        return w & m != 0
+
+#######################################################################
+
+class PvLabelT(ov.Struct):
+    '''
+       Physical Volume Label
+       ~~~~~~~~~~~~~~~~~~~~~
+       ref: #1/p56
+       ref: #2/p54
+
+       Our images evidently use a 4kB block size, but we have no
+       idea where this fact is stored, and no artifacts using a
+       different block size.
+
+       The information must be in the ``PvLabelT``, because the
+       ``lv_list`` and ``alt_lv_list`` tables are in units of the
+       block size.
+
+       It is evident from our artifacts that for SR10 the `PvLabelT`
+       has been extended relative to the documented SR9 format, but
+       no documentation covers that.
+
+    '''
+    def __init__(self, tree, lo):
+        super().__init__(
+            tree,
+            lo,
+            version_=ov.Be16,
+            apollo_=ov.Text(6),
+            name_=ov.Text(32),
+            id_=Uid,
+            f30_=ov.Be16,
+            dtype_=ov.Be16,
+            blocks_per_pvol_=ov.Be32,
+            blocks_per_track_=ov.Be16,
+            track_per_cyl_=ov.Be16,
+            lv_list_=ov.Array(10, ov.Be32, vertical=True),
+            alt_lv_list_=ov.Array(10, ov.Be32, vertical=True),
+            phys_badspot_daddr_=ov.Be32,
+            phys_diag_daddr_=ov.Be32,
+            phys_sector_start_=ov.Be16,
+            phys_sector_size_=ov.Be16,
+            pre_comp_=ov.Be16,
+
+            vertical=True,
+        )
+
+
 class ApolloDomainPhysicalVolume(ov.OctetView):
+
+    '''
+       Apollo Domain disk image
+       ~~~~~~~~~~~~~~~~~~~~~~~~
+    '''
 
     def __init__(self, this):
         if this.top not in this.parents:
             return
         if bytes(this[0x2:0x8]) != b'APOLLO':
             return
-        this.add_type("ApolloDomainFileSystem")
 
         super().__init__(this)
 
+        this.add_type("ApolloDomainPhysicalVolume")
+
         pvl = PvLabelT(self, 0).insert()
+
+        # Heuristic to guess blocksize:  Diag area is near the very end.
+        self.block_size = 1024 * int((len(this) / pvl.phys_diag_daddr.val) // 1024)
+        print(this, "Guessing blocksize is %d" % self.block_size)
+
         for lv,alt in zip(pvl.lv_list, pvl.alt_lv_list):
-            if not lv.val or alt.val <= lv.val:
-                continue
-            y = ov.This(self, lo=lv.val<<12, hi=(alt.val+1)<<12).insert()
-            y.that.add_type("ApolloDomainLogicalVolume")
-            print(lv, alt)
+            if lv.val and alt.val > lv.val:
+                y = ov.This(
+                    self,
+                    lo=lv.val * self.block_size,
+                    hi=(alt.val+1) * self.block_size,
+                ).insert()
+                y.that.add_type("ApolloDomainLogicalVolume", block_size=self.block_size)
 
         this.add_interpretation(self, this.html_interpretation_children)
         self.add_interpretation(title="ApolloDomainPhysicalVolume", more=False)
 
+#######################################################################
+
 class ApolloCoff():
     def __init__(self, this):
         if this[0] == 0x01 and this[1] == 0x97:
-            this.add_type("Apollo_68k_COFF")
+            this.add_type("COFF", system="Apollo", cpu="68k")
         elif this[0] == 0x01 and this[1] == 0x94:
-            this.add_type("Apollo_88k_COFF")
+            this.add_type("COFF", system="Apollo", cpu="88k")
+
+#######################################################################
 
 ALL = [
     ApolloDomainPhysicalVolume,
